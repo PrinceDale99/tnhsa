01dfdb1fdfcb4ca7e26d0c100b4260efab5c29e7
PrinceDale99 <99589951+PrinceDale99@users.noreply.github.com>
2023-10-30 19:21:06 +0800

    v1

---
+++ style.css
@@ -0,0 +1,45 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+.menu {
+    background-color: #333;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 200px;
+    color: #fff;
+    text-align: center;
+}
+
+.menu ul {
+    list-style: none;
+    padding: 0;
+}
+
+.menu ul li {
+    padding: 15px;
+}
+
+.menu ul li a {
+    text-decoration: none;
+    color: #fff;
+    display: block;
+}
+
+.content {
+    margin-left: 220px; /* To allow space for the menu */
+    padding: 20px;
+}
+
+/* Style for your profile at the bottom of the menu */
+.menu ul li:last-child {
+    margin-top: auto;
+    position: absolute;
+    bottom: 10px;
+    width: 100%;
+    font-style: italic;
+}
